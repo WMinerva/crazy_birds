f5f23dd0b8fc918f5a46c16505c1a35e8d37678b
WMinerva <nicolas.ochoa1697@gmail.com>
2026-01-28 18:04:00 -0400

    add drag point & respawn player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,15 +1,26 @@
 extends RigidBody2D
 
+var is_dragged = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	sleeping_state_changed.connect(_on_sleeping_state_changed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if is_dragged:
+		position = get_global_mouse_position()
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+	if event.is_action_pressed("drag"):
+		is_dragged = true
+	if event.is_action_released("drag"):
+		is_dragged = false
+		freeze = false
+
+
+func _on_sleeping_state_changed() -> void:
+	queue_free()
