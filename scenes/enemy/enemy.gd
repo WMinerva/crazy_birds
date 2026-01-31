9163876c769b4d31221b1ca04287e0476c44e19a
WMinerva <nicolas.ochoa1697@gmail.com>
2026-01-30 23:23:10 -0400

    explosion enemy

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+@export var explosion_scene: PackedScene
 @export var velocity_threshold: int
 
 
@@ -11,7 +12,19 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D):
 	# print("Contacto")
 	if body.is_in_group("player"):
-		queue_free()
+		destroy_enemy()
 		return
 	if body is RigidBody2D and body.linear_velocity.length() > velocity_threshold:
-		queue_free()
+		destroy_enemy()
+
+
+func destroy_enemy():
+	queue_free()
+	explode()
+
+
+func explode():
+	var explosion_instance = explosion_scene.instantiate()
+	# get_parent().add_child(explosion_instance)
+	add_sibling(explosion_instance)
+	explosion_instance.position = position
---
+++ scenes/explosion/explosion.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_finished.connect(_on_animation_finished)
+
+
+func _on_animation_finished():
+	queue_free()
